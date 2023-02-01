76c4c0cd79f0fa9db7706e854d6369754e8783b2
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2023-02-01 06:37:06 -0600

    run sb@next init

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+export default {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/html-webpack5",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": "tag"
+  }
+};
